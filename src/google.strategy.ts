1e11dac76f0d429b82688004ee580c8809b68136
Star-ho <58648988+Star-ho@users.noreply.github.com>
2021-04-28 00:30:18 +0900

    first commit

---
+++ src/google.strategy.ts
@@ -0,0 +1,32 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, VerifyCallback } from 'passport-google-oauth20';
+import { config } from 'dotenv';
+
+import { Injectable } from '@nestjs/common';
+
+config();
+
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+
+  constructor() {
+    super({
+        clientID: '882899233997-au7ardj1ifhpdhu9ofic3vc1d2iqhg62.apps.googleusercontent.com',
+        clientSecret: 'tx2F56_KwjByVt2Hc9gXHDk0',
+      callbackURL: 'http://localhost:3000/google/redirect',
+      scope: ['email', 'profile'],
+    });
+  }
+
+  async validate (accessToken: string, refreshToken: string, profile: any, done: VerifyCallback): Promise<any> {
+    const { name, emails, photos } = profile
+    const user = {
+      email: emails[0].value,
+      firstName: name.givenName,
+      lastName: name.familyName,
+      picture: photos[0].value,
+      accessToken
+    }
+    done(null, user);
+  }
+}
